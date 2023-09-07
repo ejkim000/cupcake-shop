2a8f3426ee193fc0292c97c2e0b06e2e38cca3af
ejkim000 <developer.ejk@gmail.com>
2023-09-06 21:12:30 -0400

    update Header & style

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,21 +1,35 @@
 import logo from '../src/assets/logo.png';
-import { FaSignInAlt } from 'react-icons/fa';
+import { FaRegUser } from 'react-icons/fa';
 
 function Header() {
   return (
     <header className="header">
+      <nav>
         <div className="logo">
-            <img src={logo} alt="cupcake-shop-logo" />
+          <img src={logo} alt="cupcake-shop-logo" />
         </div>
         <ul>
-            <li>
-              <button className="btn">
-                <FaSignInAlt /> Login
-              </button>
-            </li>
-          </ul>
+          <li>
+            <a href="#">Design</a>
+          </li>
+          <li>
+            <a href="#">BaseCake</a>
+          </li>
+          <li>
+            <a href="#">Frosting</a>
+          </li>
+          <li>
+            <a href="#">Filling</a>
+          </li>
+          <li>
+            <button className="btn round">
+              <FaRegUser />
+            </button>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
 
 export default Header;
