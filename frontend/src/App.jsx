40e77adf21a68b6cae75e0a0634e7b1a72ed7eaf
ejkim000 <developer.ejk@gmail.com>
2023-09-07 07:41:06 -0400

    add router & signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Home from '../pages/Home';
+import Signup from '../pages/Signup';
 
 function App() {
   return (
     <>
-      <div className="container">
-        <Header />
-        <Home />
-        <Footer />
-      </div>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+      <ToastContainer />
     </>
   );
 }
