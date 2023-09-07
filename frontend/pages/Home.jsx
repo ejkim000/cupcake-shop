b9a7074eb515c4f72f7ac59e506c646d19662932
ejkim000 <developer.ejk@gmail.com>
2023-09-06 21:43:22 -0400

    update Home page style

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -4,6 +4,16 @@ function Home() {
       <section className="heading">
         <h1>Welcome to Cupcake Shop</h1>
       </section>
+      <section className="bg-cupcake">
+        <div className="home-main">
+          <h2>Birth Day</h2>
+          <h2>Wedding</h2>
+          <h2>Anniversary</h2>
+          <h2>Mother&apos;s Day</h2>
+          <h2>Special Event</h2>
+          <h2><button className="btn lg center">Make Your Own Cupcake</button></h2>
+        </div>
+      </section>
     </>
   );
 }
