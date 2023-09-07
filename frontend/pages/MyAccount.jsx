4ad7dd4b75174e921d4f14c676aada7ae88e9c5f
ejkim000 <developer.ejk@gmail.com>
2023-09-07 12:28:45 -0400

    add myaccout page - empth for now

---
+++ frontend/pages/MyAccount.jsx
@@ -0,0 +1,12 @@
+function MyAccount() {
+  return (
+    <>
+      <section className="heading">
+        <h1>My Account</h1>
+        <p></p>
+      </section>
+    </>
+  );
+}
+
+export default MyAccount;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Header from '../components/Header';
 import Home from '../pages/Home';
 import Signup from '../pages/Signup';
 import Login from '../pages/Login';
+import MyAccount from '../pages/MyAccount';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/myaccount" element={<MyAccount />} />
           </Routes>
           <Footer />
         </div>
