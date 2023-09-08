eec87135947a93740361c8f1b04480eab3b254ac
ejkim000 <developer.ejk@gmail.com>
2023-09-08 18:41:59 -0400

    make option available based on the DB in the addToCartForm

--- frontend/components/AddToCartForm.jsx
+++ frontend/components/AddToCartForm.jsx
@@ -13,6 +13,14 @@ function AddToCartForm({ name, id }) {
     filling: '',
   });
   const [subTotal, setSubTotal] = useState(0);
+  const [selectedSize, setSelectedSize] = useState('');
+
+  const savedOptions = JSON.parse(
+    localStorage.getItem('cupcakeshop-selected-options')
+  );
+
+  console.log(savedOptions);
+
   const dispatch = useDispatch();
   const { items, isLoading, isError, message } = useSelector(
     (state) => state.items
@@ -21,11 +29,21 @@ function AddToCartForm({ name, id }) {
     (state) => state.sizes
   );
 
+  // set all the options by filtering state value array
+  const designSizes = sizes.filter((size) => size.item.toString() === id);
+  const cakes = items.filter((item) => item.category === 'cake');
+  const frostings = items.filter((item) => item.category === 'frosting');
+  const fillings = items.filter((item) => item.category === 'filling');
+
   const onSubmit = (e) => {
     e.preventDeafault();
   };
 
   const onChange = (e) => {
+    if (e.target.name === 'size') {
+      setSelectedSize(e.target.value);
+    }
+
     setFormData((prev) => ({
       ...prev,
       [e.target.name]: e.target.value,
@@ -42,11 +60,17 @@ function AddToCartForm({ name, id }) {
 
     dispatch(getItems());
     dispatch(getSizes());
-    console.log(items);
-    console.log(sizes);
 
-    console.log(formData, subTotal);
-  }, [formData, subTotal, dispatch, isError, isErrorS]);
+    const size = sizes.filter((s) => s._id === selectedSize);
+
+    if (size && size.length > 0) setSubTotal(size[0].price);
+
+    // save selected options in the local storage
+    localStorage.setItem(
+      'cupcakeshop-selected-options',
+      JSON.stringify(formData)
+    );
+  }, [selectedSize, dispatch, isError, isErrorS, formData]);
 
   if (isLoading || isLoadingS) {
     return <Loading />;
@@ -55,46 +79,62 @@ function AddToCartForm({ name, id }) {
   return (
     <section className="form">
       <form onSubmit={onSubmit}>
-        <div className="form-group">
+        {/* <div className="form-group">
           <label className="form-label">
             Selected Design : <strong>{name}</strong>
           </label>
+        </div> */}
+        <div className="form-group">
+          <label className="form-label">Size</label>
+          <select name="size" value={savedOptions.size} onChange={onChange} className="form-control">
+            <option value="">Please select size</option>
+            {designSizes.map((s) => (
+              <option key={s._id} value={s._id}>
+                {s.size}
+              </option>
+            ))}
+          </select>
         </div>
         <div className="form-group">
-          <label className="form-label">Select Size</label>
-          <select name="size" onChange={onChange} className="form-control">
-            {sizes &&
-              sizes
-                .filter((size) => size.item.toString() === id)
-                .map((s) => (
-                  <option key={s._id} value={s._id}>
-                    {s.size}
+          <label className="form-label">Cake</label>
+          <select name="cake" value={savedOptions.cake} onChange={onChange} className="form-control">
+            <option value="">Please select filling</option>
+            {cakes &&
+              cakes
+                // .filter((cake) => cake.size === selectedSize)
+                .map((item) => (
+                  <option key={item._id} value={item._id}>
+                    {item.name}
                   </option>
                 ))}
           </select>
         </div>
         <div className="form-group">
-          <label className="form-label">Select Cake</label>
-          <select name="cake" onChange={onChange} className="form-control">
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
+          <label className="form-label">Frosting</label>
+          <select name="frosting" value={savedOptions.frosting} onChange={onChange} className="form-control">
+            <option value="">Please select frosting</option>
+            {frostings &&
+              frostings
+                // .filter((cake) => cake.size === selectedSize)
+                .map((item) => (
+                  <option key={item._id} value={item._id}>
+                    {item.name}
+                  </option>
+                ))}
           </select>
         </div>
         <div className="form-group">
-          <label className="form-label">Select Frosting</label>
-          <select name="frosting" onChange={onChange} className="form-control">
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label className="form-label">Select Filling</label>
-          <select name="filling" onChange={onChange} className="form-control">
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
+          <label className="form-label">Filling</label>
+          <select name="filling" value={savedOptions.filling} onChange={onChange} className="form-control">
+            <option value="">No Filling</option>
+            {fillings &&
+              fillings
+                // .filter((cake) => cake.size === selectedSize)
+                .map((item) => (
+                  <option key={item._id} value={item._id}>
+                    {item.name}
+                  </option>
+                ))}
           </select>
         </div>
         <div className="form-group">
