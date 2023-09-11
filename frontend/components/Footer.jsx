544e8c982d36a0c89ad39b78ac8c947271438754
ejkim000 <developer.ejk@gmail.com>
2023-09-10 21:56:19 -0400

    update style

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,7 +1,14 @@
 function Footer() {
   return (
     <footer>
-        <p>&copy; {new Date().getFullYear()} EJ Kim </p>
+      {/* <div>
+        <ul>
+          <li>Our Story</li>
+          <li>How to Order?</li>
+          <li>Locations</li>
+        </ul>
+      </div> */}
+      <p>&copy; {new Date().getFullYear()} EJ Kim </p>
     </footer>
   )
 }
