91c0768ac17fbad7cadf12817f43a1ac4a67570b
ejkim000 <developer.ejk@gmail.com>
2023-09-12 09:09:35 -0400

    update my order model

--- backend/models/myOrderModel.js
+++ backend/models/myOrderModel.js
@@ -1,57 +1,93 @@
 const mongoose = require('mongoose');
 
-const myOrderSchema = mongoose.Schema ({
+const myOrderSchema = mongoose.Schema(
+  {
     order_no: {
-        type: String,
-        required: [true, 'Please add an order number']
+      type: String,
+      required: [true, 'Please add an order number'],
     },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
     },
-    order_items:{
-        type: Array,
-        required: true,
-        default: []
-    },
+    items: [
+      {
+        design: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Item',
+        },
+        cake: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Item',
+        },
+        frosting: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Item',
+        },
+        filling: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Item',
+        },
+        item_total: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        qty: {
+            type: Number,
+            required: true,
+            default: 1,
+        },
+        sub_total: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+      },
+    ],
     total: {
-        type: Number,
-        required: true,
-        default: 0
+      type: Number,
+      required: true,
+      default: 0,
     },
     tax: {
-        type: Number,
-        required: true,
-        default: 0
+      type: Number,
+      required: true,
+      default: 0,
     },
     grand_total: {
-        type: Number,
-        required: true,
-        default: 0
+      type: Number,
+      required: true,
+      default: 0,
     },
     status: {
-        type: String,
-        required: [true, 'Please add order status'],
-        default: "Received"
+      type: String,
+      required: [true, 'Please add order status'],
+      default: 'Received',
     },
     billing_address: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Address'
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Address',
     },
     shipping_address: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Address'
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Address',
     },
     shipping_date: {
-        type: Date,
-        default: null
-    }, 
-},
-{
-    timestamps : true
-})
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model('MyOrder', myOrderSchema);
